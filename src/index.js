f5de3379762ea7d5b233369b7e3903a4be1c9cce
Shinobi-de <mokeyddragon35@gmail.com>
2025-01-29 05:30:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Use the correct import for React 18
-
+import ReactDOM from "react-dom/client"; // React 18 import
 import App from "./App";
-import './index.css'
-
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<React.StrictMode><App /></React.StrictMode>);
